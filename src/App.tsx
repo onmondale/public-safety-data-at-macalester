2a8b9f32a755d4e52d59d96822d2f6a15b2efb54
Nelson Mondale <112660623+nmondale@users.noreply.github.com>
2024-12-04 15:45:02 -0600

    initial website draft !

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,144 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import Home from "./components/Home";
+import DataVisualizations from "./components/dataVisualizations";
+import IncidentData from "./components/IncidentData";
+import Discussion from "./components/Discussion";
+
+type Page = "home" | "dataVisualizations" | "incidentData" | "discussion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [page, setPage] = useState<Page>("home");
+  const [useStandardFonts, setUseStandardFonts] = useState(false);
+
+  const setTheme = (isDark: boolean) => {
+    setIsDarkTheme(isDark);
+    document.documentElement.style.setProperty(
+      "--background-color",
+      isDark ? "var(--dark-bg)" : "var(--light-bg)"
+    );
+    document.documentElement.style.setProperty(
+      "--text-color",
+      isDark ? "var(--dark-text)" : "var(--light-text)"
+    );
+  };
+
+  const toggleFonts = () => {
+    setUseStandardFonts(!useStandardFonts);
+    document.documentElement.style.setProperty(
+      "--font-heading",
+      !useStandardFonts ? "var(--standard-heading)" : "Phosphene, serif"
+    );
+    document.documentElement.style.setProperty(
+      "--font-body",
+      !useStandardFonts ? "var(--standard-body)" : "Garamondt, serif"
+    );
+  };
+
+  const renderComponent = () => {
+    switch (page) {
+      case "home":
+        return <Home />;
+      case "dataVisualizations":
+        return <DataVisualizations />;
+      case "incidentData":
+        return <IncidentData />;
+      case "discussion":
+        return <Discussion />;
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="headerContainer">
+        <div className="leftColumn">
+          <h1 className="title">🌀 PUBLIC SAFETY DATA AT MACALESTER COLLEGE</h1>
+          <h2 className="subtitle">
+            Making Public Safety Data Accessible and Visible
+            <br /> A Platform for Discussion
+          </h2>
+        </div>
+        <div className="rightColumn">
+          <div className="themeButtons">
+            <div className="themeToggleRow">
+              <div
+                className="toggleContainer"
+                onClick={() => setTheme(true)}
+                data-selected={isDarkTheme}
+              >
+                <span className="toggleIndicator">
+                  {isDarkTheme ? "🐦" : ""}
+                </span>
+                <span className="toggleLabel">Dark Mode</span>
+              </div>
+              <div
+                className="toggleContainer"
+                onClick={() => setTheme(false)}
+                data-selected={!isDarkTheme}
+              >
+                <span className="toggleIndicator">
+                  {!isDarkTheme ? "🐦" : ""}
+                </span>
+                <span className="toggleLabel">Light Mode</span>
+              </div>
+            </div>
+            <div
+              className="toggleContainer"
+              onClick={toggleFonts}
+              data-selected={useStandardFonts}
+            >
+              <span className="toggleIndicator">
+                {useStandardFonts ? "🐦" : ""}
+              </span>
+              <span className="toggleLabel">Enable More Legible Fonts</span>
+            </div>
+          </div>
+          <p className="description">
+            Last Updated: <br /> December 13th, 2024 at 11:13:20 AM
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      <nav className="navBar">
+        <button
+          onClick={() => setPage("home")}
+          className={`navButton ${page === "home" ? "selected" : ""}`}
+        >
+          {page === "home" && <span className="navIndicator">*</span>}
+          Home Page
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button
+          onClick={() => setPage("dataVisualizations")}
+          className={`navButton ${
+            page === "dataVisualizations" ? "selected" : ""
+          }`}
+        >
+          {page === "dataVisualizations" && (
+            <span className="navIndicator">*</span>
+          )}
+          Data Visualizations
+        </button>
+        <button
+          onClick={() => setPage("incidentData")}
+          className={`navButton ${page === "incidentData" ? "selected" : ""}`}
+        >
+          {page === "incidentData" && <span className="navIndicator">*</span>}
+          Incident Data
+        </button>
+        <button
+          onClick={() => setPage("discussion")}
+          className={`navButton ${page === "discussion" ? "selected" : ""}`}
+        >
+          {page === "discussion" && <span className="navIndicator">*</span>}
+          Discussion/Individual Reporting
+        </button>
+      </nav>
+
+      <main>{renderComponent()}</main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Discussion.tsx
@@ -0,0 +1,73 @@
+function Discussion() {
+  return (
+    <div className="contentContainer">
+      <section className="contentSection">
+        <div className="sectionRow">
+          <h2 className="sectionHeader">discussion stuff !!!</h2>
+          <div className="sectionDescription">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div className="sectionContent">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+
+      <section className="contentSection">
+        <div className="sectionRow">
+          <h2 className="sectionHeader">discussion stuff !!!?</h2>
+          <div className="sectionDescription">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div className="sectionContent">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+
+      <section className="contentSection">
+        <div className="sectionRow">
+          <h2 className="sectionHeader">discussion stuff !!!</h2>
+          <div className="sectionDescription">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div className="sectionContent">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Discussion;
