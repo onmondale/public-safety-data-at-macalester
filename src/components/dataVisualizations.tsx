094a59bff0e339592a3f53a215108f8c4ec99c33
Jonah Zimmer <jszimmer545@gmail.com>
2024-12-12 23:58:26 -0600

    progress with data visualizations

--- src/components/dataVisualizations.tsx
+++ src/components/dataVisualizations.tsx
@@ -54,6 +54,14 @@ function DataVisualizations() {
               reported at that location.
             </i>
           </p>
+          <p>
+            To create the heat map, we found the longitudes and latitudes on Google Maps of each cited location
+            from the incident report. We found, however, that there was a range of location formatting.
+            While some locations were specific buildings or fields, others were entire streets or said something as broad
+            as “off campus.” For locations that were street names, we chose a location on the map,
+            and for locations that were as broad as “off campus,” we did not include them. We had a
+            goal to include as many of the incidents as possible, and for this reason, we sacrificed some precision.
+          </p>
         </div>
       </div>
       <div className="sectionContent">
@@ -95,19 +103,24 @@ function DataVisualizations() {
         </div>
         <div className="sectionDescription" style={{ width: "100%" }}>
           <p>
-            This heatmap visualization shows the distribution and density of
-            reported incidents across the Macalester College campus from 2018 to
-            2023. The lighter areas indicate a higher concentration of reported
-            incidents, while darker areas show fewer reports.{" "}
-            <i>
-              The map is interactive, so you can zoom in and out, and click on
-              individual points to learn more about the incidents that were
-              reported at that location.
-            </i>
+            By separating the minute, hour, month, and year of each incident, we created these visualizations
+            that show different perspectives on the timing of incident reporting.{" "}
           </p>
+          <div
+            style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}
+          >
+            <img id="minuteGraph" className="timeVisualizationImg" src="/src/assets/graphs/crimePerMinuteColor.png"></img>
+            <img id="hourGraph" className="timeVisualizationImg" src="/src/assets/graphs/crimePerHourColor.png"></img>
+            <img id="monthGraph" className="timeVisualizationImg" src="/src/assets/graphs/crimePerMonthColor.png"></img>
+            <img id="yearGraph" className="timeVisualizationImg" src="/src/assets/graphs/crimePerYearColor.png"></img>
+
+          </div>
         </div>
       </div>
-      <div className="sectionContent"></div>
+      <div className="sectionContent">
+
+
+      </div>
     </div>
   );
 
@@ -130,15 +143,26 @@ function DataVisualizations() {
         </div>
         <div className="sectionDescription" style={{ width: "100%" }}>
           <p>
-            This heatmap visualization shows the distribution and density of
-            reported incidents across the Macalester College campus from 2018 to
-            2023. The lighter areas indicate a higher concentration of reported
-            incidents, while darker areas show fewer reports.{" "}
+            This visualization shows [PROVIDE MORE DESCRIPTION HERE].{" "}
             <i>
-              The map is interactive, so you can zoom in and out, and click on
-              individual points to learn more about the incidents that were
-              reported at that location.
+              The chart is interactive, so [PROVIDE MORE INSTRUCTIONS HERE!!!!]
             </i>
+          </p>
+          <p>
+            The raw data includes over one hundred unique “Incident Types.” We determined that
+            displaying this raw data in a pie chart would not be particularly informative, and
+            this was our incentive to attempt to categorize the data. We first tried to create
+            subcategories ourselves, but struggled greatly, and ultimately put the data into ChatGPT
+            to create the category headers, and decided from there which types fit into which category.
+          </p>
+          <p>
+            There were many instances where things did not fit nicely into categories. For example,
+            sexual harassment is both criminal activity as well as a form of harassment. We considered
+            allowing incidents such as this one to fall into multiple categories, but doing so would cause
+            overrepresentation of incidents that fall into multiple categories. For this reason, we decided
+            the best course of action was to make well-intentioned, but inherently biased decisions.
+            The decisions we made are easily accessible by looking at the pie chart that shows all types
+            that fall into one category, and we believe transparency helps address this issue.
           </p>
         </div>
       </div>
